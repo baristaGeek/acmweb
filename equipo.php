de01262c11f49b75cbb5994583a31ecb4c8dbc97
Esteban Vargas <estebanvargas94@gmail.com>
2015-04-16 16:15:00 -0500

    Create equipo.php

---
+++ equipo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
+-->
+<html>
+	<head>
+		<title> </title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.dropotron.min.js"></script>
+		<script src="js/jquery.scrollgress.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<?php include 'include/navbar.php'; ?>
+
+		<!-- Main -->
+			<section id="main" class="container">
+				<header>
+					<h2>Nuestro Equipo</h2>
+					<p>Conócenos</p>
+				</header>
+				<div class="box">
+					<span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+					<h3>¡Esto apenas está comenzando!</h3>
+					<p>ACM Uniandes es un grupo de estudiantes innovadores, emprendedores y trabajadores. Somos apasionados por la computación, queremos esparcir
+					el interés por ella como ciencia, profesión y arte. Estamos comprometidos con resolver los problemas de nuestra comunidad por medio de la herramienta de transformación más poderosa de todas, la tecnología. Para lograrlo, simplemente necesitamos aglomerar un grupo de gente muy inteligente.</p>
+
+			</section>
+
+			<?php include 'include/footer.php'; ?>
+
+	</body>
+</html>
