f68407a25bb0ec3dfa580e1f24cac15b1e633f2f
Esteban Vargas <estebanvargas94@gmail.com>
2015-04-16 15:48:26 -0500

    Create navbar.php

---
+++ include/navbar.php
@@ -0,0 +1,10 @@
+		<!-- Header -->
+			<header id="header" class="skel-layers-fixed">
+				<h1><a href="index.html">ACM</a> Uniandes</h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.php">Inicio</a></li>
+						<li><a href=".box special features">Nosotros</li>
+						<li><a href=".cta">Únete</li>
+				</nav>
+			</header>
