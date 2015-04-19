4d73d60d3af51f8802a100aaddc79ee1d1ddae1e
Santiago Bacaro <sbacarob@outlook.com>
2015-04-19 13:51:02 -0500

    prueba email heroku

--- email.php
+++ email.php
@@ -21,12 +21,33 @@
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-    $headers .= 'From: amigo008@hotmail.com' . "\r\n";
-    if($_POST){
+    $headers .= 'From: acm@uniandes.edu.co' . "\r\n";
+    //final
+    /*if($_POST){
         if(mail($to, $subject, $message, $headers)){
             echo "Gracias. Te hemos enviado un correo a ". $to;
         }
         else{
             echo "Lo sentimos. Hubo un error";
         }
+    }*/
+    //heroku
+    require 'vendor/autoload.php';
+    $sendgrid = new SendGrid('sbacarob', 'pruebaEmailACMUniandes');
+
+    $mail = new SendGrid\Email();
+    $mail->addTo($to)->
+        setFrom('acm@uniandes.edu.co')->
+        setSubject($subject)->
+        setText('Gracias por interesarte en nuestro capítulo. Próximamente te estaremos contactando para que puedas enterarte mejor de nuestras actividades.<')->
+        setHtml($message);
+try {
+    $sendgrid->send($mail);
+    echo "Gracias. Te hemos enviado un correo a ". $to;
+} catch(\SendGrid\Exception $e) {
+    echo $e->getCode();
+    foreach($e->getErrors() as $er) {
+        echo $er;
     }
+}
+
