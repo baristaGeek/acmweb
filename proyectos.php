d1022592bba1e8dc79ea8f2f88cfadc03337bca7
Esteban Vargas <estebanvargas94@gmail.com>
2015-04-27 10:06:41 -0500

    Update proyectos.php

--- proyectos.php
+++ proyectos.php
@@ -11,16 +11,16 @@
 			<div class="box">
 				<span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
 				<h3>Ejemplos de nuestros proyectos</h3>
-				<p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+				<p>Actualmente tenemos varios proyectos en fase de desarrollo que serán la solución a problemas de la comunidad uniandina en dominios cada vez más amplios. Estos son algunos proyectos que ya se han puesto a funcionar. </p>
 				<div class="row">
 					<div class="6u 12u(mobilep)">
 						
-						<h3>EnHueco</h3>
-						<p>Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae lorem ipsum adipiscing ac. Vivamus ornare laoreet odio vis praesent.</p>
+						<h3>Academical</h3>
+						<p><a href = "http://academical.co/">Academical</a> nació como HorarioLab en nuestro capítulo. Actualmente es una startup cuya plataforma ofrece una solución para que los estudiantes universitarios planeen su horario. En 2015, <a href = "http://www.innpulsacolombia.com/sites/default/files/informe_seleccionados_-_innpulsa_te_entrena_para_masschallenge_0.pdf">Academical hizo parte del Innpulsa Mass Challenge. </a></p>
 					</div>
 					<div class="6u 12u(mobilep)">
-						<h3>Tutorías</h3>
-						<p>Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae lorem ipsum adipiscing ac. Vivamus ornare laoreet odio vis praesent.</p>
+						<h3>Clasifiteca</h3>
+						<p>Clasifiteca es una plataforma que en sus comienzos permitía a los estudiantes intercambiar libros para sus clases. Luego su tecnología se extrapoló hacia la resolución de otros problemas, permitiendo el intercambio de cualquier tipo de artículo entre uniandinos. </p>
 					</div>
 				</div>
 			</div>
