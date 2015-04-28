d4a03c7abb85a28dfadd24d82bbfb84bf07180b9
sbacarob <sbacarob@outlook.com>
2015-04-28 09:41:00 -0500

    typos

--- actividades.php
+++ actividades.php
@@ -10,15 +10,15 @@
 			<div class="box">
 				<span class="image featured"><img src="images/PiDay.png" alt="" /></span>
 				<h3>Así esparcimos el interés por la computación</h3>
-				<p>En ACM Uniandes creamos espacios para que la comunidad se integre en torno a las ciencias de la computación. Esto incluye ambientes de aprendizaje, conferencias, celebraciones, maratones y exposiciones entre muchas otras cosas. Nuestra misión es que la computación tenga una posición más relevante en la Universidad de los Andes y es con estos espacios como obtenemso visibilidad. Estos son algunos ejemplos de lo que hacemos actualmente. </p>
+				<p>En ACM Uniandes creamos espacios para que la comunidad se integre en torno a las ciencias de la computación. Esto incluye ambientes de aprendizaje, conferencias, celebraciones, maratones y exposiciones entre muchas otras cosas. Nuestra misión es que la computación tenga una posición más relevante en la Universidad de los Andes y es con estos espacios como obtenemos visibilidad. Estos son algunos ejemplos de lo que hacemos actualmente. </p>
 				<div class="row">
 					<div class="6u 12u(mobilep)">
 						<h3>Bootcamps</h3>
-						<p>Son espacios educativos dirigidos a gente interesada en programar, sin importar el nivel. Esa persona como tú puede ser aquella que estudia Ingeniería de Sistemas y quiere complementar lo aprendido en clase, o aquella persona de otra carrera que quiere tener una introducción al tema. Los bootcamps no son una clase, son unos workshops competitivos donde conocerás gente con intereses similare para así eventualmente emprender. </p>
+						<p>Son espacios educativos dirigidos a gente interesada en programar, sin importar el nivel. Esa persona como tú puede ser aquella que estudia Ingeniería de Sistemas y quiere complementar lo aprendido en clase, o aquella persona de otra carrera que quiere tener una introducción al tema. Los bootcamps no son una clase, son unos workshops competitivos donde conocerás gente con intereses similares para así eventualmente emprender. </p>
 					</div>
 					<div class="6u 12u(mobilep)">
 						<h3>Celebración Día Pi</h3>
-						<p>El día 3/14 de cada año le hacemos honor a una de las cifras más importantes de las matemáticas. Hubo competencias de problemas matemáticos y de recitación de digitos del número, un coloquio y también comimos pie.</p>
+						<p>El día 3/14 de cada año le hacemos honor a una de las cifras más importantes de las matemáticas. Hubo competencias de problemas matemáticos y de recitación de dígitos del número, un coloquio y también comimos pie.</p>
 					</div>
 				</div>
 			</div>
