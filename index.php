d70fa6c199f2524c7a7548be9d15b09356327c75
Esteban Vargas <estebanvargas@MacBook-Air-de-Esteban.local>
2016-02-20 19:45:57 -0500

    sklfj2 git commit -mjklsfj
    git add .
    
    q
    
    heroku open
    exit

--- index.php
+++ index.php
@@ -116,7 +116,7 @@
 
 
 
-					    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -130,7 +130,7 @@
             <div class="carousel-caption">
               <h1>Example headline.</h1>
               <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              <!--p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p-->
             </div>
           </div>
         </div>
@@ -163,7 +163,8 @@
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
-    </div><!-- /.carousel -->
+       </div>
+   </div><!-- /.carousel -->
 
 
 		<!-- CTA -->
