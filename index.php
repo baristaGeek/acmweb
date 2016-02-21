d820a8141888a81126d4ef1c9d859c340fd788b5
Esteban Vargas <estebanvargas@MacBook-Air-de-Esteban.local>
2016-02-20 19:35:26 -0500

    sjklfj

--- index.php
+++ index.php
@@ -114,27 +114,45 @@
 				</div>
 
 				<div class = "row">
-					<div class = "12u">
-						<section class = "box">
-							<div id = "myCarousel" class = "carousel slide" data-ride = "carousel">
-							<h2>Ofertas de Trabajo</h2>
-							<h3>Última Oferta: Fronted Dev</h3>
-							<p>Se requiere blah blah blah...</p>
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
 
-							  <ol class="carousel-indicators">
-    							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    							<li data-target="#myCarousel" data-slide-to="1"></li>
-    							<li data-target="#myCarousel" data-slide-to="2"></li>
-    							<li data-target="#myCarousel" data-slide-to="3"></li>
-  								</ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img_chania.jpg" alt="Chania">
+    </div>
 
+    <div class="item">
+      <img src="img_chania2.jpg" alt="Chania">
+    </div>
 
-							</div>
-						</section>
-					</div>
+    <div class="item">
+      <img src="img_flower.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="img_flower2.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 				</div>
-
-			</section>
 
 		<!-- CTA -->
 			<section id="cta">
